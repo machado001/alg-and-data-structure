291c2b0a23c83be027e91671e1bcda5d6785959d
machado001 <machadowg@gmail.com>
2022-09-07 20:13:55 -0300

    aula 02

--- Recursividade/hanoi.js
+++ Recursividade/hanoi.js
@@ -5,4 +5,4 @@ function hanoi(num_discos, torre_A, torre_B, torre_C) {
         hanoi(num_discos - 1, torre_C, torre_B, torre_A)
     }
 }
-hanoi(4, 'A', 'B', 'C')
+hanoi(4, 'A', 'B', 'C') // expect todos os passos sequenciais para resolver a torre. sabe-se que o número mínimo de movimentos para completar a torre é ( (2^num_discos) - 1 ), aqui seria então 15 movimentos mínimos.
